0c15b3fa202f89f0b2ba3efb6037a7e0d369a9ea
Ricardo Ribeiro <ralexandre11.gmail.com>
2021-01-29 19:21:11 -0300

    Project stated

---
+++ src/main/java/com/ribeiro/mercadoEletronicoApi/MercadoEletronicoApiApplication.java
@@ -0,0 +1,13 @@
+package com.ribeiro.mercadoEletronicoApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MercadoEletronicoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MercadoEletronicoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ribeiro/mercadoEletronicoApi/MercadoEletronicoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ribeiro.mercadoEletronicoApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MercadoEletronicoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
