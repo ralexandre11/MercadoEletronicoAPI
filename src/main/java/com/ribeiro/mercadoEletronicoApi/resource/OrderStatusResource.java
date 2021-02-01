2e7437df7322ed38ea6ffd4e6da712ae3882b198
Ricardo Ribeiro <ralexandre11.gmail.com>
2021-01-31 23:10:01 -0300

    Add Order status Resource

--- src/main/java/com/ribeiro/mercadoEletronicoApi/resource/OrderStatusResource.java
+++ src/main/java/com/ribeiro/mercadoEletronicoApi/resource/OrderStatusResource.java
@@ -1,17 +1,20 @@
 package com.ribeiro.mercadoEletronicoApi.resource;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.ribeiro.mercadoEletronicoApi.exception.PersonalException;
 import com.ribeiro.mercadoEletronicoApi.model.entity.Order;
-import com.ribeiro.mercadoEletronicoApi.resource.dto.OrderDTO;
+import com.ribeiro.mercadoEletronicoApi.model.enums.Status;
 import com.ribeiro.mercadoEletronicoApi.resource.dto.OrderStatusDTO;
+import com.ribeiro.mercadoEletronicoApi.resource.dto.StatusResponseDTO;
 import com.ribeiro.mercadoEletronicoApi.service.OrderService;
 
 import lombok.RequiredArgsConstructor;
@@ -25,15 +28,39 @@ public class OrderStatusResource {
 	private final OrderService service;
 
 	@PostMapping
-	public ResponseEntity<OrderStatusDTO> save(@RequestBody final OrderStatusDTO statusDTO) {
-		try {
-			final Order order = service.ConvertDTOEntity(orderDTO);
-			final Order orderSaved = service.save(order);
-			final OrderDTO orderDTOSaved = service.ConvertEntityDTO(orderSaved);
-			return new ResponseEntity<OrderStatusDTO>(orderDTOSaved, HttpStatus.CREATED);
-		} catch (final PersonalException e) {
-			return ResponseEntity.badRequest().body(e.getMessage());
-		}
+	public ResponseEntity<StatusResponseDTO> verifyStatusOrder(@RequestBody final OrderStatusDTO statusDTO) {
+		StatusResponseDTO statusResponse = new StatusResponseDTO();
+		statusResponse.setPedido(statusDTO.getPedido());
+		statusResponse.setStatus(checkStatus(statusDTO));
+		return ResponseEntity.ok().body(statusResponse);
 	}
+
+	public List<Status> checkStatus(OrderStatusDTO statusDTO) {
+		List<Status> status = new ArrayList<>();
+		Optional<Order> orderOptional = service.getByNumber(statusDTO.getPedido());
+		if (!orderOptional.isPresent()) {
+			status.add(Status.CODIGO_PEDIDO_INVALIDO);
+		} else {
+			Order order = orderOptional.get();
+			if (statusDTO.getStatus() == "REPROVADO") {
+				status.add(Status.REPROVADO);
+			} else if (statusDTO.getValorAprovado().compareTo(order.getTotal()) == 0  && statusDTO.getItensAprovados() == order.getTotalItems()) {
+				status.add(Status.APROVADO);
+			} else {
+				if (statusDTO.getValorAprovado().compareTo(order.getTotal()) > 0) {
+					status.add(Status.APROVADO_VALOR_A_MAIOR);
+				} else {
+					status.add(Status.APROVADO_VALOR_A_MENOR);
+				}
+				if (statusDTO.getItensAprovados() > order.getTotalItems()) {
+					status.add(Status.APROVADO_QTD_A_MAIOR);
+				} else {
+					status.add(Status.APROVADO_QTD_A_MENOR);
+				}
+			}
+		}
+		return status;
+	}
+	
 	
 }
